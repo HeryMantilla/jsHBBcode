8ae4f1652131b0ca0af372036b2ccdacf25a026f
HeryMantilla <108959409+HeryMantilla@users.noreply.github.com>
2022-07-08 22:26:23 -0500

    Add files via upload

---
+++ jshlang.js
@@ -0,0 +1,11 @@
+if (typeof lang === 'undefined') { lang = []; }
+
+lang['Spoiler'] = 'Spoiler'; // comes before bbcode spoiler output.
+lang['Quote'] = 'Quote'; // comes before bbcode quote output.
+lang['wrote'] = 'wrote'; // proceeds the username and is followed by the quoted text
+lang['Code'] = 'Code'; // comes before bbcode code output.
+lang['free'] = '';
+lang['Mod'] = '';
+lang['OpenExtLink'] = 'Open in External Link';
+lang['UserOnlineOK'] = 'Please register if you want to read this message.';
+lang['Mod_warninig'] = 'Moderator warning';
